d6af2f26ad0a924f3e69ebf78e2bcf96def4bf9d
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-03-22 21:12:02 -0600

    agrega función para leer contenido de los cursos al darle click

--- js/app.js
+++ js/app.js
@@ -13,7 +13,25 @@ function cargarEventListeners() {
 // Funciones
 function agregarCurso(e) {
   e.preventDefault();
+  e.target.classList;
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log(e.target);
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
   }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso) {
+  console.log(curso);
+
+  // Crear un Objeto con el contenido del curso actual
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    autor: curso.querySelector("p").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+  console.table(infoCurso);
 }
