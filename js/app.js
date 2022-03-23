7ae218128d99e534a8b684700f87eb28a3152000
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-03-22 21:54:42 -0600

    agrega destructuring en el forEach para recorrer articulosCarrito

--- js/app.js
+++ js/app.js
@@ -29,7 +29,6 @@ function leerDatosCurso(curso) {
   const infoCurso = {
     imagen: curso.querySelector("img").src,
     titulo: curso.querySelector("h4").textContent,
-    autor: curso.querySelector("p").textContent,
     precio: curso.querySelector(".precio span").textContent,
     id: curso.querySelector("a").getAttribute("data-id"),
     cantidad: 1,
@@ -49,11 +48,14 @@ function carritoHTML() {
 
   // Recorre el carrito y genera el HTML
   articulosCarrito.forEach((curso) => {
+    const { imagen, titulo, precio, cantidad, id } = curso;
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>
-        ${curso.titulo}
-      </td>
+      <td> <img src="${imagen}" alt="curso imagen" width=100/> </td>
+      <td>${titulo}</td>
+      <td>${precio}</td>
+      <td>${cantidad}</td>
+      <td> <a href="#" class="borrar-curso data-id="${id}"> X </a> </td>
     `;
 
     // Agrega el HTML del carrito en el body
